121e911f89e9aceb8b8ef54b61a555e8a5fa3e15
6chaoran <6chaoran@gmail.com>
2024-03-27 08:42:28 +0800

    update area page

--- components/ReviewModal.vue
+++ components/ReviewModal.vue
@@ -1,40 +1,49 @@
 <template>
   <TransitionRoot as="template" :show="open">
     <Dialog as="div" class="relative z-10" @close="open = false">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       </TransitionChild>
-      <div class="fixed top-20 inset-0 z-10 w-screen overflow-y-auto">
+      <div class="fixed top-[8rem] inset-0 z-10 w-screen overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="w-full transform overflow-auto rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">              
-              <div>x</div>
+          <TransitionChild as="template" enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel
+              class="w-full transform overflow-auto rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
               <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                   <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900 ">Google Maps Review</DialogTitle>
+                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900 ">Google Maps Review
+                    </DialogTitle>
                     <div class="mt-2">
-                        <ul role="list" class="divide-y divide-gray-100">
-                            <li v-for="(review, i) in reviews" class="text-left text-sm text-gray-500 gap-x-6 py-5" :key="i">
-                              <v-col class="-mb-2">
-                                <v-row>
-                                <strong>{{ review.username }}</strong> 
-                                <v-spacer></v-spacer>{{ datediff(review.timestamp) }}
-                              </v-row>
-                              <v-row class="mb-1"><v-rating :model-value="review.rating" 
-                                :size="14" :length="5" color="yellow-darken-2" density="comfortable" readonly></v-rating></v-row>
-                              <v-row>{{ review.caption }}</v-row>
-                              </v-col>
-                      </li>
-                        </ul>
+                      <ul role="list" class="divide-y divide-gray-100">
+                        <li v-for="(review, i) in reviews" class="text-left text-sm text-gray-500 gap-x-6 py-5"
+                          :key="i">
+                          <v-col class="-mb-2">
+                            <v-row>
+                              <strong>{{ review.username }}</strong>
+                              <v-spacer></v-spacer>{{ datediff(review.timestamp) }}
+                            </v-row>
+                            <v-row class="mb-1"><v-rating :model-value="review.rating" :size="14" :length="5"
+                                color="yellow-darken-2" density="comfortable" readonly></v-rating></v-row>
+                            <v-row>{{ review.caption }}</v-row>
+                          </v-col>
+                        </li>
+                      </ul>
 
                     </div>
                   </div>
                 </div>
-                
+
               </div>
               <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="open = false" ref="cancelButtonRef">Close</button>
+                <button type="button"
+                  class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  @click="open = false" ref="cancelButtonRef">Close</button>
               </div>
             </DialogPanel>
           </TransitionChild>
@@ -48,33 +57,31 @@
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { $on } from 'vue-happy-bus';
 const open = ref(false)
-$on('showReviews', () => {
-    open.value = true
-})
-const props = defineProps({
-    schoolId: String,
-})
-
 const reviews = ref(null)
-useFetch(`/api/review/${props.schoolId}`).then((resp) => {
-  reviews.value = resp.data.value
+const props = defineProps({
+  schoolId: String,
 })
-
+$on('showReviews', () => {
+  useFetch(`/api/review/${props.schoolId}`).then((resp) => {
+    reviews.value = resp.data.value
+    open.value = true
+  })
+})
 const datediff = (date) => {
   const today = new Date()
   const ts = new Date(date)
   const diff = new Date(today.getTime() - ts.getTime())
   const days = diff.getUTCDate()
-  const months = diff.getUTCMonth() 
+  const months = diff.getUTCMonth()
   const years = diff.getUTCFullYear() - 1970
   // console.log(`${ts} ${years} ${months} ${days}`)
-  if (years == 0 && months == 0 && days < 7){
+  if (years == 0 && months == 0 && days < 7) {
     const unit = days == 1 ? 'day' : 'days'
     return `${days} ${unit} ago`
   } else if (years == 0 && months == 0) {
     const weeks = (days / 7).toFixed(0)
     const unit = weeks == 1 ? 'week' : 'weeks'
-    return `${weeks} ${unit} ago` 
+    return `${weeks} ${unit} ago`
   } else if (years == 0) {
     const unit = months == 1 ? 'month' : 'months'
     return `${months} ${unit} ago`
--- pages/area/[area].vue
+++ pages/area/[area].vue
@@ -1,14 +1,18 @@
 <template>
-            <v-breadcrumbs :items="items" color="indigo-darken-3" divider="/"> <span class="text-gray-600">&nbsp;&nbsp;/</span>
-            <template v-slot:prepend>
-                <v-icon size="small" icon="mdi-home" color="indigo-600"></v-icon>
-            </template>
-        </v-breadcrumbs>
-<ul role="list" class="divide-y divide-gray-100">
+  <div class="-mx-4">
+    <v-breadcrumbs :items="items" color="indigo-darken-3" divider="/"> <span class="text-gray-600">&nbsp;&nbsp;/</span>
+      <template v-slot:prepend>
+        <v-icon size="small" icon="mdi-home" color="indigo-600"></v-icon>
+      </template>
+    </v-breadcrumbs>
+  </div>
+  <div>{{ schoolsForDisplay.length }} schools in total</div>
+  <ul role="list" class="divide-y divide-gray-100">
     <li v-for="school in schoolsForDisplay" :key="school.school_id" class="relative flex justify-between gap-x-6 py-5">
       <div class="flex gap-x-4">
         <img class="h-12 w-12 flex-none rounded-full bg-gray-50" loading="lazy" width="48px" height="48px"
-          :src="school.logo_url2 ? '/school_logo/' + school.logo_url2 : school.logo_url" :alt="'logo of ' + school.name" />
+          :src="school.logo_url2 ? '/school_logo/' + school.logo_url2 : school.logo_url"
+          :alt="'logo of ' + school.name" />
         <div class="min-w-0 flex-auto">
           <p class="text-sm font-semibold leading-6 text-gray-900">
             <a :href="`/schools/${school.school_id}`">
@@ -33,23 +37,27 @@
             </span>
             <span v-if="school['affiliations']"
               class="inline-flex items-center rounded-full bg-blue-50 mx-2 px-2 py-1 text-xs font-medium text-green-600 ring-1 ring-inset ring-green-500/10">
-              Affil. 
+              Affil.
             </span>
-            <v-tooltip v-if="school['affiliations']" activator="parent" location="right">{{ school['affiliations'] }}</v-tooltip>
+            <v-tooltip v-if="school['affiliations']" activator="parent" location="right">{{ school['affiliations']
+              }}</v-tooltip>
 
           </p>
           <div class="mt-1 flex items-center gap-x-1.5 sm:hidden">
             <div class="flex-none rounded-full bg-emerald-500/20 p-1">
               <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
             </div>
-            <p class="text-xs leading-5 text-gray-500">{{ school.area }} |</p> 
+            <p class="text-xs leading-5 text-gray-500">{{ school.area }} |</p>
             <div class="flex items-center">
-              <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+              <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor" viewBox="0 0 22 20">
+                <path
+                  d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
               </svg>
               <!-- rating -->
-              <p class="text-xs text-gray-500 dark:text-white">{{ school.review_rating_avg ? school.review_rating_avg.toFixed(2) : 'N/A'   }}</p>
-                   </div>
+              <p class="text-xs text-gray-500 dark:text-white">{{ school.review_rating_avg ?
+      school.review_rating_avg.toFixed(2) : 'N/A' }}</p>
+            </div>
           </div>
           <p class="mt-1 flex text-xs leading-5 text-gray-500">
             {{ school.address }}
@@ -89,7 +97,7 @@ const id = route.params.area
 const { data } = await useFetch('/api/list_school')
 const schools = ref(data.value.filter(x => x.school_id != 'juying'))
 const schoolsForDisplay = computed(() => {
-    return schools.value.filter((x) => x.area.toLowerCase() == id)
+  return schools.value.filter((x) => x.area.toLowerCase() == id)
 })
 
 const items = computed(() => {
@@ -100,10 +108,11 @@ const items = computed(() => {
       href: '/',
     },
     {
-      title: id[0].toUpperCase() + id.slice(1).toLowerCase(),
+      title: schoolsForDisplay.value[0].area,
       disabled: true,
       href: '/',
     }
-]})
+  ]
+})
 
 </script>

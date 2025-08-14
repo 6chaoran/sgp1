d4398e00809eb20784835e37f8acc467ec941efe
6chaoran <6chaoran@gmail.com>
2025-08-14 21:12:47 +0800

    init

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    firebase: {
+      gen: 2
+    }
+  },
   runtimeConfig: {
     public:{
       supabaseKey: process.env.NUXT_SUPABASE_KEY,
